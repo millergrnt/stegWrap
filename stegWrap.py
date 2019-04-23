b21d3b413d0affedf9b68cd8143b506caefdf2e7
Jacob Luke Chrzanowski <jlc3467@rit.edu>
2019-04-23 00:38:33 -0400

    Add files via upload

--- stegWrap.py
+++ stegWrap.py
@@ -1,73 +1,81 @@
 #!/usr/bin/python3
 import subprocess
 from subprocess import PIPE,STDOUT
+import stegWrap_file_text as fileText
+
+def mainMenu():
+    print("************************************")
+    print("*        Welcome to Stegwrap       *")
+    print("************************************")
+    print(" Stegwrap has several tools at your disposal: ")
+    print(" [1] Ankylfind - Steghide's brute forcing counter part")
+    print("                    Locked behind an annoying password? Just break it!")
+    print(" [2] <INSERT ENCRYPTION NAME HERE>")
+    print(" [3] Hide text/data/files & recover file menu")
+    print(" [4] Exit Stegwrap")
+    print(" Please choose the tool you wish to use: ", end="")
+
 
 def ankylFindUsage():
-	print("*****************************")
-	print("*    Welcome to Ankylfind   *")
-	print("*****************************")
-	print("\nExample:")
-	print("\t\tankylFind.py [options] inImage1 inImage2 ...")
-	print("\nOptions:")
-	print("\t\t-h/--help\tPrint this message")
-	print("\t\t-v/--verbose\tVerbose output")
-	print("\t\t-w/--wordlist\tWordlist to draw passwords from")
-	print("\t\t-b/--brute-force\tBrute force generate the password")
-	print("\t\t-s/--smallest\t\tSmallest possible password")
-	print("\t\t-l/--largest\t\tLargest possible password")
-	print("\t\t-u/--use-symbols\tUse symbols in brute force generation")
+    print("*****************************")
+    print("*    Welcome to Ankylfind   *")
+    print("*****************************")
+    print("\nExample:")
+    print("\t\tankylFind.py [options] inImage1 inImage2 ...")
+    print("\nOptions:")
+    print("\t\t-h/--help\tPrint this message")
+    print("\t\t-v/--verbose\tVerbose output")
+    print("\t\t-w/--wordlist\tWordlist to draw passwords from")
+    print("\t\t-b/--brute-force\tBrute force generate the password")
+    print("\t\t-s/--smallest\t\tSmallest possible password")
+    print("\t\t-l/--largest\t\tLargest possible password")
+    print("\t\t-u/--use-symbols\tUse symbols in brute force generation")
 
 def main():
 
-	# Present welcome banner and menu
-	print("************************************")
-	print("*        Welcome to Stegwrap       *")
-	print("************************************")
-	print("Stegwrap has several tools at your disposal: ")
-	print("[1] Ankylfind - Steghide's brute forcing counter part")
-	print("                   Locked behind an annoying password? Just break it!")
-	print("[2] <INSERT ENCRYPTION NAME HERE>")
-	print("[3] Exit Stegwrap")
-	print("Please choose the tool you wish to use: ", end="")
+    # Show menu of choices to user.
+    mainMenu()
 
-	ankylFind = "./ankylfind.py"
-	encryption = "./encryption.py"
+    ankylFind = "./ankylfind.py"
+    encryption = "./encryption.py"
 
-	# Keep asking user for their choice of tool
-	choice = input()
-	while "3" not in choice:
+    # Keep asking user for their choice of tool
+    choice = input()
+    while choice[0] != "4":
 
-		if "1" in choice:
+        if choice[0] == "1":
 
-			# Print ankylfind usage for user
-			ankylFindUsage()
+            # Print ankylfind usage for user
+            ankylFindUsage()
 
-			print("Please enter command arguments: ", end="")
-			commands = input()
-			ankylFindCommand = []
-			ankylFindCommand.append(ankylFind)
-			commands = commands.split()
+            print("Please enter command arguments: ", end="")
+            commands = input()
+            ankylFindCommand = []
+            ankylFindCommand.append(ankylFind)
+            commands = commands.split()
 
-			for option in commands:
-				ankylFindCommand.append(option)
+            for option in commands:
+                ankylFindCommand.append(option)
 
-			process = subprocess.Popen(ankylFindCommand, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-			out = (process.communicate()[0]).decode()
-			print(out)
+            process = subprocess.Popen(ankylFindCommand, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+            out = (process.communicate()[0]).decode()
+            print(out)
 
-		elif "2" in choice:
-			process = subprocess.Popen([encryption])
-		else:
-			print("Unknown option chosen")
+        elif choice[0] == "2":
+            process = subprocess.Popen([encryption])
+        elif choice[0] == "3":
+            fileText.dialog() # Hiding and extracting files manually
+        else:
+            print("Unknown option chosen")
 
-		print("[1] Ankylfind")
-		print("[2] <INSERT ENCRYPTION NAME HERE>")
-		print("[3] Exit")
-		print("Please choose the tool you wish to use: ", end="")
-		choice = input()
+        
+        # Show menu of choices to user.
+        mainMenu()
 
-	print("Thank you for using Stegwrap")
+        choice = input()
+
+    print("Thank you for using Stegwrap")
 
 
 if __name__ == '__main__':
-	main()
+    main()
